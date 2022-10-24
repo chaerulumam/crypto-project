f68c68f5ca656a8e514b422a6ffbd115d00763e1
Chaerul Umam <chaerulumamb244@gmail.com>
2022-10-24 23:00:23 +0700

    add coin route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./routes/Home";
 import Signin from "./routes/Signin";
 import Signup from "./routes/Signup";
 import Account from "./routes/Account";
+import CoinPage from "./routes/CoinPage";
 
 function App() {
   return (
@@ -16,6 +17,9 @@ function App() {
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/account" element={<Account />} />
+        <Route path="/coin/:coinId" element={<CoinPage />}>
+          <Route path="/:coinId" />
+        </Route>
       </Routes>
     </ThemeProvider>
   );
