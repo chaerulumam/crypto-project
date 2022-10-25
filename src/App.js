6ca3071148ffabb2bcca1af3610a8e1cf529d95a
Chaerul Umam <chaerulumamb244@gmail.com>
2022-10-25 19:57:38 +0700

    add footer section

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Signin from "./routes/Signin";
 import Signup from "./routes/Signup";
 import Account from "./routes/Account";
 import CoinPage from "./routes/CoinPage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path=":coinId" />
         </Route>
       </Routes>
+      <Footer />
     </ThemeProvider>
   );
 }
