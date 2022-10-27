cb595ff9abc1df5f08b74414c384a450e9f6a453
Chaerul Umam <chaerulumamb244@gmail.com>
2022-10-27 20:15:18 +0700

    instegrasi navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ThemeToggle from "./ThemeToggle";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
+  const { user, logout } = UserAuth();
+  const navigate = useNavigate();
 
   const handleNav = () => {
     setNav(!nav);
   };
+
+  const handleSignOut = async () => {
+    try {
+      await logout();
+      navigate("/");
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
   return (
     <div className="rounded-div flex items-center justify-between h-20 font-bold ease-in duration-300 z-10">
       <Link to="/">
@@ -17,17 +30,26 @@ const Navbar = () => {
       <div className="hidden md:block">
         <ThemeToggle />
       </div>
-      <div className="hidden md:block">
-        <Link to="/signin" className="p-4 hover:text-accent">
-          Sign In
-        </Link>
-        <Link
-          to="/signup"
-          className="bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl"
-        >
-          Sign Up
-        </Link>
-      </div>
+      {user?.email ? (
+        <div>
+          <Link to="/account" className="p-4">
+            Account
+          </Link>
+          <button onClick={handleSignOut}>Sign Out</button>
+        </div>
+      ) : (
+        <div className="hidden md:block">
+          <Link to="/signin" className="p-4 hover:text-accent">
+            Sign In
+          </Link>
+          <Link
+            to="/signup"
+            className="bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl"
+          >
+            Sign Up
+          </Link>
+        </div>
+      )}
       {/* Menu Icon */}
       <div onClick={handleNav} className="block md:hidden cursor-pointer z-10">
         {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
